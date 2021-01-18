6037ff809f2da931b1edc7873c3178bc056d00b3
Hongze Xia <hongze.x@gmail.com>
2021-01-18 16:57:58 +1100

    feat: async copy resp to file

--- src/http.rs
+++ src/http.rs
@@ -3,8 +3,11 @@ use crate::options::Opts;
 // https://query1.finance.yahoo.com/v7/finance/download/GXY.AX?period1=1579236638&period2=1610859038&interval=1d&events=history&includeAdjustedClose=true
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-use std::fs::File;
-use std::io::Write;
+use tokio::fs::File;
+use tokio::io;
+// Needed for the stream conversion
+use futures::stream::{StreamExt, TryStreamExt};
+use hyper::{Response, Body};
 
 // #[allow(dead_code)]
 pub async fn download(opts: Opts) -> Result<()> {
@@ -19,28 +22,37 @@ pub async fn download(opts: Opts) -> Result<()> {
             base.as_str(),
             &[
                 ("period1", start.as_str()),
-                ("period2",end.as_str()),
+                ("period2", end.as_str()),
                 ("includeAdjustedClose", opts.adjusted_close.to_string().as_str()),
                 ("events", "history"),
                 ("interval", "1d"),
             ],
         )
-        .unwrap();
+            .unwrap();
         println!("{}", url.as_str());
         let uri = url.into_string().parse()?;
         let resp = client.get(uri).await?;
         println!("headers are {:?}", resp.headers());
         println!("status is {:?}", resp.status());
-        let body = hyper::body::to_bytes(resp).await?;
-        println!("body len is {}, {:?}...", body.len(), body);
         {
-            let mut file = File::create("test.csv")?;
-            // Write a slice of bytes to the file
-            file.write(&body)?;
+            // had to use into_body because after consuming the body resp is not going to be used
+            // if using body(), a ref is used but resp is moved so it won't compile
+            let futures_io_async_read = resp
+                .into_body()
+                .map(|result| result.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string())))
+                .into_async_read();
+            let mut tokio_async_read = tokio_util::compat::FuturesAsyncReadCompatExt::compat(futures_io_async_read);
+
+            let mut file = File::create("test.csv").await?;
+            io::copy(&mut tokio_async_read, &mut file).await?;
         }
     }
     Ok(())
 }
+
+// async fn write_to_file(resp: Response<Body>) {
+//
+// }
 
 #[cfg(test)]
 mod tests {
