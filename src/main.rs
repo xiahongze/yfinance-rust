e9f31aa578c194876cef0a638f7db06352811045
Hongze Xia <hongze.x@gmail.com>
2021-01-25 21:38:02 +1100

    feat: separate cmd handling in main for more clarity

--- src/main.rs
+++ src/main.rs
@@ -32,35 +32,39 @@ fn walk_dir<P: AsRef<Path>>(dir: P, recursive: bool) -> Box<dyn Iterator<Item = 
     }
 }
 
+fn convert(path: PathBuf) {
+    match load_from_json(path.as_path().to_str().unwrap()) {
+        Ok(chart_wrapper) => {
+            let ds_vec: Vec<DataSet> = chart_wrapper.chart.into();
+
+            let outputs = if ds_vec.len() == 1 {
+                vec![path.with_extension("csv")]
+            } else {
+                let stem = path
+                    .file_stem()
+                    .map_or("unknown_stem", |s| s.to_str().unwrap())
+                    .to_string();
+                (0..ds_vec.len())
+                    .map(|i| path.with_file_name(format!("{:?}_{}.csv", stem, i)))
+                    .collect()
+            };
+
+            ds_vec.iter().zip(outputs.iter()).for_each(|(ds, path)| {
+                if let Err(err) = write_to_csv(ds, path) {
+                    error!("failed to write to csv {:?} with {:?}", path, err);
+                } else {
+                    info!("successfully converted to {:?}", path);
+                }
+            });
+        }
+        Err(err) => error!("failed to load json from {:?} with {:?}", path, err),
+    }
+}
+
 fn convert_to_csv(json_dir: &String, recursive: bool) -> std::io::Result<()> {
     walk_dir(json_dir, recursive)
         .filter(|path| path.extension().map_or(false, |ext| ext == "json"))
-        .for_each(|path| match load_from_json(path.to_str().unwrap()) {
-            Ok(chart_wrapper) => {
-                let ds_vec: Vec<DataSet> = chart_wrapper.chart.into();
-
-                let outputs = if ds_vec.len() == 1 {
-                    vec![path.with_extension("csv")]
-                } else {
-                    let stem = path
-                        .file_stem()
-                        .map_or("unknown_stem", |s| s.to_str().unwrap())
-                        .to_string();
-                    (0..ds_vec.len())
-                        .map(|i| path.with_file_name(format!("{:?}_{}.csv", stem, i)))
-                        .collect()
-                };
-
-                ds_vec.iter().zip(outputs.iter()).for_each(|(ds, path)| {
-                    if let Err(err) = write_to_csv(ds, path) {
-                        error!("failed to write to csv {:?} with {:?}", path, err);
-                    } else {
-                        info!("successfully converted to {:?}", path);
-                    }
-                });
-            }
-            Err(err) => error!("failed to load json from {:?} with {:?}", path, err),
-        });
+        .for_each(convert);
     Ok(())
 }
 
@@ -68,7 +72,7 @@ fn convert_to_csv(json_dir: &String, recursive: bool) -> std::io::Result<()> {
 async fn main() {
     env_logger::init();
     let opts = options::parse();
-    let (json_dir, convert, recursive) = match opts.subcmd {
+    match opts.subcmd {
         SubCommand::Download(opts) => {
             if let Some(start) = opts.start {
                 if let Some(end) = opts.end {
@@ -77,15 +81,18 @@ async fn main() {
                     }
                 }
             }
-            let _ = http::download(&opts).await;
-            (opts.output_dir, opts.convert, false)
+            let results = http::download(&opts).await;
+            if opts.convert {
+                results.into_iter().for_each(|(p, res)| {
+                    if res.is_ok() {
+                        convert(p);
+                    }
+                });
+            }
         }
-        SubCommand::Convert(opts) => (opts.input_dir, true, opts.recursive),
-    };
-    if convert {
-        match convert_to_csv(&json_dir, recursive) {
-            Err(err) => error!("failed to walk dir {} with {:?}", json_dir, err),
+        SubCommand::Convert(opts) => match convert_to_csv(&opts.input_dir, opts.recursive) {
+            Err(err) => error!("failed to walk dir {} with {:?}", opts.input_dir, err),
             _ => {}
-        }
-    }
+        },
+    };
 }
